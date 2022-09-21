2b5a5fdf621f3ca26a7c652d50aa616a10ab13e0
murderuo <ugurokur365@hotmail.com>
2022-09-21 14:58:44 +0300

    react logic started

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,6 +1,46 @@
 import MainStyle from '../../Styles/Main.module.css';
+import { useState } from 'react';
+import axios from 'axios';
+const TOKEN = process.env.REACT_APP_BEARER_TOKEN;
 
 function Main() {
+  const [inputValue, setInputValue] = useState('');
+  const [error, setError] = useState('');
+
+  const handleInputChange = e => {
+    // console.log(e.target.value);
+    setInputValue(e.target.value);
+    setError('');
+  };
+
+  const handleInputSubmit = () => {
+    if (inputValue.includes('twitter.com')) return getVideoUrl(inputValue);
+    setError('Lütfen geçerli bir twitter linki giriniz.');
+    // setUrl(inputValue);
+  };
+
+  const getVideoUrl = async inputValue => {
+    const _token = TOKEN;
+    const tweetInfo = await urlCheck(inputValue);
+    const header = { Authorization: _token };
+    const requestUrl = `https://api.twitter.com/1.1/statuses/show/${tweetInfo.tweetId}.json?&tweet_mode=extended`;
+    const response = await axios.get(requestUrl, { headers: header });
+    const videoUrl =
+      response.data.extended_entities.media[0].video_info.variants;
+    console.log(videoUrl);
+  };
+
+  const urlCheck = input => {
+    //https://twitter.com/i/status/1572542444176289794
+    const tweetData = { user: '', tweetId: '' };
+
+    const tweetUrl = inputValue.split('?', 1)[0];
+    // console.log(tweetUrl);
+    tweetData.user = tweetUrl.split('/')[3];
+    tweetData.tweetId = tweetUrl.split('/')[5];
+    return tweetData;
+  };
+
   return (
     <>
       <main className={MainStyle.main}>
@@ -10,7 +50,10 @@ function Main() {
             <input
               type="text"
               placeholder="Video bağlantınızı buraya yapıştırın"
+              value={inputValue}
+              onChange={handleInputChange}
             />
+            <h3>{error}</h3>
             <h5>
               Hizmetimizi kullanarak Hizmet Şartlarımızı ve Gizlilik
               Politikamızı kabul etmiş olursunuz
@@ -18,9 +61,9 @@ function Main() {
           </div>
           <div className={MainStyle.content}>
             <h2>Medya dosyalarını indirmek için Şimdi indir' e tıklayın!</h2>
-            <button>
+            <button onClick={handleInputSubmit}>
               Şimdi İndir
-              <i class="fa-regular fa-circle-down" />
+              <i className="fa-regular fa-circle-down" />
             </button>
           </div>
         </div>
